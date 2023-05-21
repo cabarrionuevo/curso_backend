c58b90ececb80a977684ba5ea158c44cfc0e9cc4
Charly <cbarrionuevo@fidelatam.com>
2023-05-21 12:17:18 -0300

    Relacion entre user y task Ok Eagerloading Ok

--- base_de_datos/controllers/tasks.js
+++ base_de_datos/controllers/tasks.js
@@ -1,14 +1,23 @@
 //Importamos el modelo Task
 const Task = require('../models').Task;
+const User = require('../models').User;
 
 module.exports = {
   index: function(req,res){
     Task.findAll().then((tasks)=>{
-      res.render('tasks/index',{tasks: tasks});
+      res.render('tasks/index',{tasks: req.user.tasks});
     })
   },
   show: function(req,res){
-    Task.findByPk(req.params.id).then(function(task){      
+    Task.findByPk(req.params.id,{
+      //EAGERLOADING - P/INDICAR CARGA DE MODELOS RELACIONADOS
+      include: [
+        {
+          model: User,
+          as:'user'
+        },
+      ]
+    }).then(function(task){      
       //Opción 1
       // res.render('tasks/show',{task:task});
       //Opción 2 usando short hand property, cuando la clave es igual al valor
@@ -34,7 +43,8 @@ module.exports = {
   },
   create: function(req,res){
       Task.create({
-        description: req.body.description
+        description: req.body.description,
+        userId:req.user.id
       }).then(result =>{ 
         res.json(result);
     }).catch(err=>
--- base_de_datos/middlewares/find_user.js
+++ base_de_datos/middlewares/find_user.js
@@ -1,10 +1,16 @@
+const { Association } = require('sequelize');
+
 const User = require('../models').User;
 
 module.exports = function(req,res,next){
     
     console.log("**************************",req.session.userId);
     if(!req.session.userId) return next();    
-    User.findByPk(req.session.userId).then(user=>{        
+    User.findByPk(req.session.userId,{
+        include:[{
+            association: 'tasks'
+        }]
+    }).then(user=>{        
         if(user){
             req.user = user;
             next();
